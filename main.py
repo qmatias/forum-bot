df7967cdd362b1b33727779bb70df92266cc9ae9
Matias Kotlik <mdkotlik@gmail.com>
2020-09-01 15:26:27 -0500

    inital commit

---
+++ main.py
@@ -0,0 +1,22 @@
+import discord
+from discord.ext import commands
+from configparser import ConfigParser
+from os import path
+
+# Root project directory
+ROOT_DIR = path.dirname(path.abspath(__file__))
+
+CONFIG = ConfigParser()
+try:
+    CONFIG.read(path.join(ROOT_DIR, 'config.ini'))
+    API_KEY = CONFIG['main']['token']
+except Exception:
+    raise IOError('Error reading config file')
+
+bot = commands.Bot(command_prefix='>')
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send('pong')
+
+bot.run(API_KEY)
