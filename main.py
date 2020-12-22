579beb96d8bf4830c88f49b85d606897189deb44
NirajSalunkhe <47925956+NirajSalunkhe@users.noreply.github.com>
2020-12-21 19:21:10 -0600

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from discord import Message
 from discord.ext import commands
 from configparser import ConfigParser
 from os import path
+from discord.ext.commands import Context
+
 # TODO: at some point, move to peewee-async for theoretical, probably completely insignificant performance gains
 #  https://peewee-async.readthedocs.io/en/latest/
 from peewee import *
@@ -13,38 +15,76 @@ ROOT_DIR = path.dirname(path.abspath(__file__))
 CONFIG = ConfigParser()
 try:
     CONFIG.read(path.join(ROOT_DIR, 'config.ini'))
-    API_KEY = CONFIG['main']['token']
-    DATABASE_FILE = CONFIG['main']['databaseurl']
+    API_KEY = CONFIG['discord']['token']
+    DATABASE_FILE = CONFIG['database']['url']
 except Exception:
     raise IOError('Error reading config file')
-db = SqliteDatabase(DATABASE_FILE)
+db = SqliteDatabase(DATABASE_FILE, pragmas={'foreign_keys': 1})
+
+
+class User(Model):
+    discord_id = BigIntegerField()
+
+    class Meta:
+        database = db
 
 
 class Thread(Model):
-    author = CharField()  # TODO: add actual author model and link to it here
+    author = ForeignKeyField(User, backref='threads')
+    discord_guild = BigIntegerField()
     created = DateField()
+    subject = TextField()
+    content = TextField()
+
+    class Meta:
+        database = db
+
+
+class Comment(Model):
+    author = ForeignKeyField(User, backref='comments')
+    thread = ForeignKeyField(Thread, backref='comments')
+    created = DateField()
+    content = TextField()
 
     class Meta:
         database = db
 
 
 db.connect()
-db.create_tables([Thread])
-Thread.create(author='Joe', created=date(1960, 1, 15))
+db.create_tables([Thread, User, Comment])
+myUser1 = User.create(discord_id=101)
+myUser2 = User.create(discord_id=69)
+thread1 = Thread.create(author=myUser1, created=date(1960, 1, 15), discord_guild=101, subject="hello world",
+                        content="hi")
+Comment.create(author=myUser2, created=date(1969, 2, 12), thread=thread1, content="suh dude")
 
 # print out all the threads we have in the database
 for thread in Thread.select():
-    print(f'{thread.created} {thread.author}')
+    print(f'{thread.created} {thread.author.discord_id} {thread.content}')
 
 CHANNEL = "support-channel"
 if __name__ == '__main__':
-    bot = commands.Bot(command_prefix='>')
+    bot = commands.Bot(command_prefix='!')
 
 
-    @bot.event
-    async def on_message(message: Message):
-        if message.channel.name == CHANNEL:
-            print(f'Message sent in support channel: {message.content} from {message.author.name}')
+    def channel_command(*args, **kwargs):
+        def _channel_command(func):
+            @bot.command(*args, **kwargs)
+            async def __channel_command(ctx: Context):
+                if ctx.channel.name == CHANNEL:
+                    return await func()
+
+            return __channel_command
+
+        return _channel_command
+
+
+    @channel_command()
+    async def ping(ctx: Context):
+        msg = ""
+        for thread in Thread.select():
+            msg += (f'{thread.created} {thread.author.discord_id} {thread.content} {thread.subject}\n')
+        await ctx.send(msg)
 
 
     bot.run(API_KEY)
